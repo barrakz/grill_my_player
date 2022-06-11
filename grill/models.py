4d724a533e796ddd52540da34888c92f6ca28cc3
barrakz <b.rakuzy@gmail.com>
2022-06-11 16:58:14 +0200

    Basic models, add app to settings and register to admin view

--- grill/models.py
+++ grill/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+
+class Match(models.Model):
+    teams = models.CharField(max_length=50)
+    date = models.DateField()
+    score = models.CharField(max_length=5)
+
+    def __str__(self):
+        return f"{self.teams}"
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=50)
+    match = models.ForeignKey(Match, on_delete=models.CASCADE, related_name="match")
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birth_date = models.IntegerField()
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="players")
+
+    def __str__(self):
+        return f"{self.name} {self.last_name}"
+
+
+class Rating(models.Model):
+    rate = models.IntegerField()
+    player = models.ForeignKey(Player, on_delete=models.CASCADE, related_name="ratings")
+
+    def __str__(self):
+        return f"{self.rate}"
+
+
+class Comment(models.Model):
+    comment_text = models.CharField(max_length=1000)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE, related_name="comments")
+
+    def __str__(self):
+        return f"{self.comment_text}"
+
