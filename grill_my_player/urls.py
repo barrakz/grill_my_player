9ec2204f829990edea11e1a3c236871e0b4fcdc8
marckraw <marckraw@icloud.com>
2022-06-11 13:45:02 +0200

    Edit urls to point into proper views

--- grill_my_player/urls.py
+++ grill_my_player/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 from grill import views
 
-app_name = "polls_api"
+app_name = "grill_api"
 
 urlpatterns = [
-    path("questions/", views_api.QuestionList.as_view(), name="questions"),
-    path("questions/<pk>/", views_api.QuestionDetail.as_view(), name="question-detail"),
-    path("answers/", views_api.AnswerList.as_view(), name="answers"),
-    path("answers/<pk>/", views_api.AnswerDetail.as_view(), name="answer-detail")
+    path("players/", views.PlayerList.as_view(), name="players"),
+    path("players/<pk>/", views.PlayerDetail.as_view(), name="player-detail"),
 ]
 
 
