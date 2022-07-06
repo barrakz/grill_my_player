baa243a80187283b82738e69eb0120cf12d8a36f
barrakz <b.rakuzy@gmail.com>
2022-07-06 21:48:56 +0200

    fix allowed_hosts

--- grill_my_player/settings.py
+++ grill_my_player/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qb+yp%#=vj&jnnrihch=*sroo3_vh-gz0m-oeg9#f622nu3w87
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
