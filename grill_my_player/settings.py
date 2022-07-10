f8f4e49958d6ba9100ad66a18034e78abe3bfebe
marckraw <marckraw@icloud.com>
2022-07-10 13:21:43 +0200

    Remove path from cors allowed origins

--- grill_my_player/settings.py
+++ grill_my_player/settings.py
@@ -57,8 +57,8 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'https://grill-my-player.herokuapp.com/',
-    'https://grill-my-player.vercel.app/'
+    'https://grill-my-player.herokuapp.com',
+    'https://grill-my-player.vercel.app'
 ]
 
 ROOT_URLCONF = 'grill_my_player.urls'
