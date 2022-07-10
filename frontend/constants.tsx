fcbbdfe9e798b38463a472d00b38c3c09baa2795
marckraw <marckraw@icloud.com>
2022-07-10 13:46:41 +0200

    Fix not existing auth table

--- frontend/constants.tsx
+++ frontend/constants.tsx
@@ -1,2 +1,5 @@
-export const BASE_BACKEND_URL = 'http://localhost:8000'
+const PROD = 'https://grill-my-player.herokuapp.com'
+const DEV = 'http://localhost:8000'
+export const BASE_BACKEND_URL = PROD
 export const BASE_REST_URL= `${BASE_BACKEND_URL}/models-rest`
+
--- grill_my_player/settings.py
+++ grill_my_player/settings.py
@@ -86,12 +86,17 @@ WSGI_APPLICATION = 'grill_my_player.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# ie if Heroku server
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 
 # Password validation
