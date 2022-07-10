6fc13b7a34fc6119d10b15200ca9eb254f3f38c0
marckraw <marckraw@icloud.com>
2022-07-10 14:02:13 +0200

    Remove default database ?

--- grill_my_player/settings.py
+++ grill_my_player/settings.py
@@ -94,12 +94,12 @@ WSGI_APPLICATION = 'grill_my_player.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
